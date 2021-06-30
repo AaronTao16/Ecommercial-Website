15957746a8f1bf0c255740efb11bbdfeb4ee01ae
AaronTao16 <aarontao16@outlook.com>
2021-06-30 18:23:17 -0400

    build module

---
+++ products/src/test/java/com/yit/ecommercial/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yit.ecommercial.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ware/src/test/java/com/yit/ecommercial/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yit.ecommercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
